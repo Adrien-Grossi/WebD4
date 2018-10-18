915c2be64e04d45aaf34e1afda903e3eb19043cd
Adrien-Grossi <43069391+Adrien-Grossi@users.noreply.github.com>
2018-10-18 16:18:33 +0200

    3eme commit

--- autostopvideo.js
+++ autostopvideo.js
@@ -1,9 +1,9 @@
 $('.carousel-control-prev').click(function() {
   $('#AdriensVideo').carousel('prev');
-    $("video").each(function () { this.pause(); this.currentTime = 0;  });
+    $("Video").each(function () { this.pause(); this.currentTime = 0;  });
 });
 
 $('.carousel-control-next').click(function() {
   $('#AdriensVideo').carousel('next');
-  $("video").each(function () { this.pause(); this.currentTime = 0; });
+  $("Video").each(function () { this.pause(); this.currentTime = 0; });
 });
